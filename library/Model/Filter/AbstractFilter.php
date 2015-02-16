644b772af4bc40c42fc73d67197af8dc52bb8560
RuSPanzer <ruspanzer@gmail.com>
2015-02-16 15:06:03 +0300

    fix

--- library/Model/Filter/AbstractFilter.php
+++ library/Model/Filter/AbstractFilter.php
@@ -4,6 +4,12 @@ namespace Model\Filter;
 
 abstract class AbstractFilter
 {
+    protected $options;
+
+    public function setOptions($option = array())
+    {
+        $this->options = $option;
+    }
 
     /**
      * @param $value
